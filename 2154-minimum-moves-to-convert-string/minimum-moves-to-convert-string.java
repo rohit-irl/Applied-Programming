380ad7ee2530b55e81236408a4762bb1e65c0e0b
Rohit <r.raj14112006@gmail.com>
2026-03-09 12:32:05 +0530

    Time: 1 ms (76.20%) | Memory: 43.1 MB (11.52%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int moves = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'X'){
+                moves++;
+                i += 2;
+            }
+        }
+        return moves;
+    }
+}
