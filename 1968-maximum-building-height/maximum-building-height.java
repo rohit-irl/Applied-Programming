24f3362aeea03b38b3cab799cce6545818907a40
Rohit <r.raj14112006@gmail.com>
2026-06-20 10:22:28 +0530

    Time: 55 ms (73.53%) | Memory: 124.2 MB (47.06%) - LeetSync

---
+++ 1968-maximum-building-height/maximum-building-height.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxBuilding(int n, int[][] restrictions) {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 0});
+
+        for (int[] r : restrictions) {
+            list.add(r);
+        }
+        Collections.sort(list, (a, b) -> a[0] - b[0]);
+        int m = list.size();
+
+        for (int i = 1; i < m; i++) {
+            list.get(i)[1] = Math.min(
+                list.get(i)[1],
+                list.get(i - 1)[1] + (list.get(i)[0] - list.get(i - 1)[0])
+            );
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            list.get(i)[1] = Math.min(
+                list.get(i)[1],
+                list.get(i + 1)[1] + (list.get(i + 1)[0] - list.get(i)[0])
+            );
+        }
+        int ans = 0;
+
+        for (int i = 1; i < m; i++) {
+            int id1 = list.get(i - 1)[0];
+            int h1 = list.get(i - 1)[1];
+            int id2 = list.get(i)[0];
+            int h2 = list.get(i)[1];
+            int dist = id2 - id1;
+            ans = Math.max(ans, (h1 + h2 + dist) / 2);
+        }
+        int lastId = list.get(m - 1)[0];
+        int lastHeight = list.get(m - 1)[1];
+        ans = Math.max(ans, lastHeight + (n - lastId));
+        return ans;
+    }
+}
